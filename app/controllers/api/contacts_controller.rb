00cf5faf6c7559acaf9198e53a97aa2ec6ca00ec
Brian <riceb53@gmail.com>
2018-06-25 20:59:17 -0500

    add one contact url and all contacts url

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -1,2 +1,11 @@
 class Api::ContactsController < ApplicationController
+  def one_contact_action
+    @contact = Contact.first
+    render "one_contact_view.json.jbuilder"
+  end
+
+  def all_contacts_action
+    @contacts = Contact.all
+    render "all_contacts_view.json.jbuilder"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # STEP 1: A ROUTE triggers a controller action
   # verb "/urls" => "namespace/controllers#action"
+  get "/one_contact_url" => "api/contacts#one_contact_action"
+  get "/all_contacts_url" => "api/contacts#all_contacts_action"
 end
